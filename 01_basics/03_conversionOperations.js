f7eae18a4d4d849fd3ded22c2d667d79678e05b3
devAmay11 <amayvaidya112@email.com>
2024-12-09 15:19:12 +0530

    comparison changes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,39 @@
+let score = "33"
+console.log(typeof(score));
+
+let valueInNumber = Number (score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+// *********************** operations ************************
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+// Postfix increment
+
+// let x = 3;
+// const y = x++;
+// x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+// Prefix increment
+// let x = 3;
+// const y = ++x;
+// x is 4; y is 4
+
+// let x2 = 3n;
+// const y2 = ++x2;
+// x2 is 4n; y2 is 4n
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log("2" === 2); 
+// Checks if both the value and type are the same.
+console.log("2" !== 2); 
+// Checks if both the value and type are not equal.
