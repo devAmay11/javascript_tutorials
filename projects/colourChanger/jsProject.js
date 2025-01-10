84f4390d512f1f91a4b666548d62ba56d491e135
devAmay11 <amayvaidya112@email.com>
2025-01-10 12:39:17 +0530

    projects

---
+++ projects/colourChanger/jsProject.js
@@ -0,0 +1,12 @@
+const button = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+button.forEach((color) =>{
+    color.addEventListener('click',(e)=>{
+        const gotColorId = e.target.id
+        if(gotColorId){
+            body.style.backgroundColor = gotColorId;
+
+        }
+
+    })
+})
